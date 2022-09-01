c6f7a2a152e95adb3cb1cf10d1dbb7a6da7ed499
MayankBorkar <mayankborkar190@gmail.com>
2022-09-01 12:35:59 +0530

    done with Anylizing target app.

--- App.js
+++ App.js
@@ -1,20 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {ImageBackground, SafeAreaView, StyleSheet} from 'react-native';
+import StartGameScreen from "./screen/StartGameScreen";
+import {LinearGradient} from 'expo-linear-gradient';
+import {useState} from "react";
+import GameScreen from "./screen/GameScreen";
+import colors from "./constants/colors";
+import GameOverScreen from "./screen/GameOverScreen";
+import {useFonts} from "expo-font";
+import AppLoading from 'expo-app-loading';
+import {StatusBar} from "expo-status-bar";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const [userNumber, setUserNumber] = useState();
+    const [gameIsOver, setGameIsOver] = useState(true);
+    const [guessRounds, setGuessRounds] = useState(0);
+
+    const [fontLoaded] = useFonts({
+        'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+        'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+    });
+
+    if (!fontLoaded) {
+        return <AppLoading/>;
+    }
+
+    function pickedNumberHandler(pickedNumber) {
+        setUserNumber(pickedNumber);
+        setGameIsOver(false);
+    }
+
+    function gameOverHandler(numberOfRounds) {
+        setGameIsOver(true);
+        setGuessRounds(numberOfRounds);
+    }
+
+    function startNewGameHandler() {
+        setUserNumber(null);
+        setGuessRounds(0);
+
+    }
+
+    let screen = <StartGameScreen onPickNumber={pickedNumberHandler}/>;
+
+    if (userNumber) {
+        screen = <GameScreen userNumber={userNumber} onGameOver={gameOverHandler}/>;
+    }
+
+    if (gameIsOver && userNumber) {
+        screen =
+            <GameOverScreen userNumber={userNumber} roundsNumber={guessRounds} onStartNewGame={startNewGameHandler}/>
+    }
+
+    return (
+        <>
+            <StatusBar style="light"/>
+            <LinearGradient colors={[colors.primary700, colors.accent500]}
+                            style={styles.rootScreen}
+            >
+                <ImageBackground source={require('./assets/background.png')}
+                                 resizeMode="cover"
+                                 style={styles.rootScreen}
+                                 imageStyle={styles.backgroundImage}
+                >
+                    <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
+                </ImageBackground>
+            </LinearGradient>
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    rootScreen: {
+        flex: 1
+    },
+    backgroundImage: {
+        opacity: 0.15,
+    },
 });
---
+++ components/game/NumberContainer.js
@@ -0,0 +1,35 @@
+import {Text, View, StyleSheet, Dimensions} from "react-native";
+import colors from "../../constants/colors";
+
+
+function NumberContainer({children}) {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.numberText}>
+                {children}
+            </Text>
+        </View>
+    );
+}
+
+export default NumberContainer;
+
+const deviceWidth = Dimensions.get('window').width;
+
+const styles = StyleSheet.create({
+    container: {
+        borderWidth: 4,
+        borderColor: colors.accent500,
+        padding: deviceWidth < 380 ? 12 : 24,
+        borderRadius: 8,
+        margin: deviceWidth < 380 ? 12 : 24,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    numberText: {
+        color: colors.accent500,
+        fontSize: deviceWidth < 380 ? 28 : 36,
+        // fontWeight: 'bold',
+        fontFamily: 'open-sans-bold'
+    }
+});
